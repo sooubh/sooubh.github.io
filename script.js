ca12eab5ac796ae627a5766b89ab8c7c2071e3fe
sourabh <sourabh3527@gmail.com>
2025-04-13 22:47:59 +0530

    Upadte from mobile

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function hideSidebar(){
 }
 // End of sidebar toggle functionality
 
+let circleProgressElements = document.querySelectorAll('.circle-progress');
+let progressValueElements = document.querySelectorAll('.progress-value'); 
+
+let progressStartValues = [0, 0, 0, 0, 0];
+let progressEndValues = [90, 78, 70, 60, 85];
+let speed = 50; 
+
+circleProgressElements.forEach((circleProgress, index) => {
+ let progress = setInterval(() => {
+progressStartValues[index]++;
+progressValueElements[index].textContent = `${progressStartValues[index]}%`;
+circleProgress.style.background = `conic-gradient(#13e30c ${progressStartValues[index] * 3.6}deg, #bbb5b57e 0deg)`; 
+
+if (progressStartValues[index] == progressEndValues[index]) {
+clearInterval(progress);
+}
+}, speed);
+});
